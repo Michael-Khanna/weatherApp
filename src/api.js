edea1ca71cdb09fb82538cd18c4270cc470c58f1
Michael-Khanna <mikekhanna@icloud.com>
2021-08-04 17:44:29 -0700

    Changed link from http to https

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 let getWeather = async function (city) {
   let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=13bc0708dbf016035c6f343a866e5a6d`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=13bc0708dbf016035c6f343a866e5a6d`
   );
 
   let data = response.json(); //extracts JSON body (what we want)
